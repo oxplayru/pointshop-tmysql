aa5938ff2e21c6e15354f60df36b60a46ab344cc
Alex Mantis <BaMTTuP@gmail.com>
2016-02-09 04:14:56 +0300

    Update to new version of tmysql

--- lua/providers/tmysql.lua
+++ lua/providers/tmysql.lua
@@ -1,6 +1,6 @@
 --[[
 
-    PointShop tMySQL Adapter by Spencer Sharkey
+    PointShop tMySQL Adapter by Spencer Sharkey and fixed by Vampir
     Should work, and has verbose output.
 
     Adapted from _Undefined's pointshop-mysql
@@ -33,36 +33,37 @@ if (err) then
 else
     function PROVIDER:GetData(ply, callback)
         local qs = string.format("SELECT * FROM `pointshop_data` WHERE uniqueid='%s'", ply:UniqueID())
-        db:Query(qs, function(res, pass, err)
-            if (not pass) then ErrorNoHalt("[PSMySQL-GetData] "..err) return end
+        db:Query(qs, function(res)
+            if (not res[1].status) then ErrorNoHalt("[PSMySQL-GetData] "..res[1].error) return end
             if (#res < 1) then callback(0, {}) return end
-            local row = res[1]
-            print("[PSMySQL-GetData] "..ply:UniqueID())
+            
+            local row = res[1].data[1]
+            --print("[PSMySQL-GetData] "..ply:UniqueID())
             callback(row.points or 0, util.JSONToTable(row.items or '{}'))
         end, QUERY_FLAG_ASSOC)
     end
 
     function PROVIDER:SetPoints(ply, points)
         local qs = string.format("INSERT INTO `pointshop_data` (uniqueid, points, items) VALUES ('%s', '%s', '[]') ON DUPLICATE KEY UPDATE points = VALUES(points)", ply:UniqueID(), points or 0)
-        db:Query(qs, function(res, pass, err)
-            if (not pass) then ErrorNoHalt("[PSMySQL-SetPoints] "..err) return end
-            print("[PSMySQL-SetPoints] "..ply:UniqueID().."="..points)
+        db:Query(qs, function(res)
+            if (not res[1].status) then ErrorNoHalt("[PSMySQL-SetPoints] "..res[1].error) return end
+         --   print("[PSMySQL-SetPoints] "..ply:UniqueID().."="..points)
         end, QUERY_FLAG_ASSOC)
     end
 
     function PROVIDER:GivePoints(ply, points)
         local qs = string.format("INSERT INTO `pointshop_data` (uniqueid, points, items) VALUES ('%s', '%s', '[]') ON DUPLICATE KEY UPDATE points = points + VALUES(points)", ply:UniqueID(), points or 0)
-        db:Query(qs, function(res, pass, err)
-            if (not pass) then ErrorNoHalt("[PSMySQL-GivePoints] "..err) return end
-            print("[PSMySQL-GivePoints] "..ply:UniqueID().."+="..points)
+        db:Query(qs, function(res)
+            if (not res[1].status) then ErrorNoHalt("[PSMySQL-GivePoints] "..res[1].error) return end
+          --  print("[PSMySQL-GivePoints] "..ply:UniqueID().."+="..points)
         end, QUERY_FLAG_ASSOC)
     end
 
     function PROVIDER:TakePoints(ply, points)
         local qs = string.format("INSERT INTO `pointshop_data` (uniqueid, points, items) VALUES ('%s', '%s', '[]') ON DUPLICATE KEY UPDATE points = points - VALUES(points)", ply:UniqueID(), points or 0)
-        db:Query(qs, function(res, pass, err)
-            if (not pass) then ErrorNoHalt("[PSMySQL-TakePoints] "..err) return end
-            print("[PSMySQL-TakePoints] "..ply:UniqueID().."-="..points)
+        db:Query(qs, function(res)
+            if (not res[1].status) then ErrorNoHalt("[PSMySQL-TakePoints] "..res[1].error) return end
+          --  print("[PSMySQL-TakePoints] "..ply:UniqueID().."-="..points)
         end, QUERY_FLAG_ASSOC)
     end
 
@@ -73,29 +74,29 @@ else
     function PROVIDER:GiveItem(ply, item_id, data)
         local tmp = table.Copy(ply.PS_Items)
         tmp[item_id] = data
-        local qs = string.format("INSERT INTO `pointshop_data` (uniqueid, points, items) VALUES ('%s', '0', '%s') ON DUPLICATE KEY UPDATE items = VALUES(items)", ply:UniqueID(), tmysql.escape(util.TableToJSON(tmp)))
-        db:Query(qs, function(res, pass, err)
-            if (not pass) then ErrorNoHalt("[PSMySQL-GiveItem] "..err) return end
-            print("[PSMySQL-GiveItem] "..ply:UniqueID().."="..item_id)
+        local qs = string.format("INSERT INTO `pointshop_data` (uniqueid, points, items) VALUES ('%s', '0', '%s') ON DUPLICATE KEY UPDATE items = VALUES(items)", ply:UniqueID(), db:Escape(util.TableToJSON(tmp)))
+        db:Query(qs, function(res)
+            if (not res[1].status) then ErrorNoHalt("[PSMySQL-GiveItem] "..res[1].error) return end
+         --   print("[PSMySQL-GiveItem] "..ply:UniqueID().."="..item_id)
         end, QUERY_FLAG_ASSOC)
     end
 
     function PROVIDER:TakeItem(ply, item_id)
         local tmp = table.Copy(ply.PS_Items)
         tmp[item_id] = nil
-        local qs = string.format("INSERT INTO `pointshop_data` (uniqueid, points, items) VALUES ('%s', '0', '%s') ON DUPLICATE KEY UPDATE items = VALUES(items)", ply:UniqueID(), tmysql.escape(util.TableToJSON(tmp)))
-        db:Query(qs, function(res, pass, err)
-            if (not pass) then ErrorNoHalt("[PSMySQL-TakeItem] "..err) return end
-            print("[PSMySQL-TakeItem] "..ply:UniqueID().."="..item_id)
+        local qs = string.format("INSERT INTO `pointshop_data` (uniqueid, points, items) VALUES ('%s', '0', '%s') ON DUPLICATE KEY UPDATE items = VALUES(items)", ply:UniqueID(), db:Escape(util.TableToJSON(tmp)))
+        db:Query(qs, function(res)
+            if (not res[1].status) then ErrorNoHalt("[PSMySQL-TakeItem] "..res[1].error) return end
+           -- print("[PSMySQL-TakeItem] "..ply:UniqueID().."="..item_id)
         end, QUERY_FLAG_ASSOC)
     end
 
     function PROVIDER:SetData(ply, points, items)
-        local qs = string.format("INSERT INTO `pointshop_data` (uniqueid, points, items) VALUES ('%s', '%s', '%s') ON DUPLICATE KEY UPDATE points = VALUES(points), items = VALUES(items)", ply:UniqueID(), points or 0, tmysql.escape(util.TableToJSON(items)))
+        local qs = string.format("INSERT INTO `pointshop_data` (uniqueid, points, items) VALUES ('%s', '%s', '%s') ON DUPLICATE KEY UPDATE points = VALUES(points), items = VALUES(items)", ply:UniqueID(), points or 0, db:Escape(util.TableToJSON(items)))
 
-        db:Query(qs, function(res, pass, err)
-            if (not pass) then ErrorNoHalt("[PSMySQL-SetData] "..err) return end
-            print("[PSMySQL-SetData] "..ply:UniqueID())
+        db:Query(qs, function(res)
+            if (not res[1].status) then ErrorNoHalt("[PSMySQL-SetData] "..res[1].status) return end
+           -- print("[PSMySQL-SetData] "..ply:UniqueID())
         end, QUERY_FLAG_ASSOC)
     end
 end
